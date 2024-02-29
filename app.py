a977b37363cd6ca4e888d26dc715aff7160d8ebf
Shukdev Datta <99469701+shukdevtroy@users.noreply.github.com>
2024-02-29 11:27:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ from dotenv import load_dotenv
 import os
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS #facebook AI similarity search
 from langchain.chains.question_answering import load_qa_chain
 from langchain import HuggingFaceHub
-import concurrent.futures
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask InnovBot")
-    
     # Add logo image
     logo_path = 'Innovation4.png'
     st.image(logo_path, width=200)
-    
-    st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+    # Add voice greeting
+    voice_path = 'greeting.mp3'
+    st.audio(voice_path, format='audio/mp3', start_time=0)
 
     st.header("Ask InnovBot")
     
@@ -30,37 +32,29 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        # Split the text into chunks
-        chunk_size = 1000
-        chunk_overlap = 200
-        chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size - chunk_overlap)]
+        # spilit ito chuncks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
 
-        # Create embedding
+        # create embedding
         embeddings = HuggingFaceEmbeddings()
 
-        # Create knowledge base
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        knowledge_base = FAISS.from_texts(chunks,embeddings)
 
         user_question = st.text_input("Ask Question:")
         if user_question:
-            # Search for similar documents
             docs = knowledge_base.similarity_search(user_question)
+            llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":5,
+                                                      "max_length":500})
+            chain = load_qa_chain(llm,chain_type="stuff")
+            response = chain.run(input_documents=docs,question=user_question)
 
-            # Load question answering chain
-            llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":5, "max_length":500})
-            chain = load_qa_chain(llm, chain_type="stuff")
-
-            # Process the first chunk only
-            response = process_chunk(docs[0], chain, user_question)
-
-            # Display response
             st.write(response)
-
-def process_chunk(doc, chain, user_question):
-    # Process chunk
-    response = chain.run(input_documents=[doc], question=user_question)
-
-    return response
 
 if __name__ == '__main__':
     main()
